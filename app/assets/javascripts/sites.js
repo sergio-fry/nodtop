d3deb3d41488fae9a2e51e63d97034b79350b7a7
Sergei O. Udalov <roger.wilco.from.sq@gmail.com>
2014-02-04 20:02:54 +0400

    % fixed

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -7,7 +7,7 @@ $(function() {
     table.find(".site").each(function(){
       var share = parseFloat($(this).data("rating")) / rating_total;
       console.log(share);
-      $(this).find(".share").text(Math.round(share * 100) / 100 + " %");
+      $(this).find(".share").html(Math.round(share * 10000) / 100 + "&nbsp;%");
     });
   });
 });
