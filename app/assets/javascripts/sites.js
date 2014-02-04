f369c24662c7d275f839df11d9aee9bf7344290c
Sergei O. Udalov <roger.wilco.from.sq@gmail.com>
2014-02-04 20:00:40 +0400

    рассчет % охвата от общего НОД

---
+++ app/assets/javascripts/sites.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(".sites-table").each(function() {
+    var table = $(this);
+    var rating_total = 0;
+    table.find(".site").each(function(){ rating_total += parseFloat($(this).data("rating")) });
+
+    table.find(".site").each(function(){
+      var share = parseFloat($(this).data("rating")) / rating_total;
+      console.log(share);
+      $(this).find(".share").text(Math.round(share * 100) / 100 + " %");
+    });
+  });
+});
