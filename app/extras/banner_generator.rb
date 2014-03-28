f4d424f21925d85091f3ea38d9765d586ac1d29a
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-03-28 21:05:26 +0400

    banner orange text

--- app/extras/banner_generator.rb
+++ app/extras/banner_generator.rb
@@ -10,7 +10,7 @@ class BannerGenerator
     watermark_text.annotate(image, 0,0,2,0, "#{count}-й") do
       watermark_text.gravity = EastGravity
       self.pointsize = 13
-      self.fill = "white"
+      self.fill = "#ed7500"
       self.font_family = "Arial"
       self.font_weight = BoldWeight
       self.stroke = "none"
