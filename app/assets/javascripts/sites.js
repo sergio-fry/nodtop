7d50ee6fedfbdfc6f4d3f691b4cdb60c9082e9d1
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-04-04 08:56:59 +0400

    галки при регистрации

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -9,4 +9,24 @@ $(function() {
       $(this).find(".share").html(Math.round(share * 10000) / 100 + "&nbsp;%");
     });
   });
+
+  $("#new_site_form").each(function() {
+    var submit_button = $(this).find(":input[type='submit']").attr("disabled", "disabled");
+    var form = $(this);
+
+    var update_submit = function() {
+      // когда все галки стоят - разрешаем регистрацию
+      if(form.find(":input:checkbox:checked").length == form.find(":input:checkbox").length) {
+        submit_button.attr("disabled", null);
+      } else {
+        submit_button.attr("disabled", "disabled");
+      }
+    }
+
+    update_submit();
+
+    $(this).find(":input[type='checkbox']").change(function() {
+      update_submit();
+    });
+  });
 });
