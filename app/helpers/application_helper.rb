9d58edfbd5b010671df39ec11b6d7b6ee95be4c3
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-04-07 12:33:46 +0400

    график на главной - ширина

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,22 @@ module ApplicationHelper
     if metric.blank? || metric.data_set.blank?
       "Нет данных"
     else
-      concat content_tag(:div, nil, :id => "metric_graph_#{metric.id}")
+      content_tag :div do
+        concat content_tag(:div, nil, :id => "metric_graph_#{metric.id}", :style => "width: 100%")
 
-      concat javascript_tag <<-JS
-      g = new Dygraph(
+        concat javascript_tag <<-JS
+        g = new Dygraph(
 
-        // containing div
-        document.getElementById("metric_graph_#{metric.id}"),
+          // containing div
+          document.getElementById("metric_graph_#{metric.id}"),
 
-        // CSV or path to a CSV file.
-        "Date,#{title || metric.title}\\n" +
+          // CSV or path to a CSV file.
+          "Date,#{title || metric.title}\\n" +
 
-        #{raw metric.data_set.sort_by{ |d| d[:date] }.map{ |d| "\"#{d[:date].strftime("%Y-%m-%d %H:%M")},#{d[:value]}\\n\"" }.join(" + ")}
-      );
-      JS
+          #{raw metric.data_set.sort_by{ |d| d[:date] }.map{ |d| "\"#{d[:date].strftime("%Y-%m-%d %H:%M")},#{d[:value]}\\n\"" }.join(" + ")}
+        );
+        JS
+      end
     end
   end
 end
