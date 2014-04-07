32d7b67791886a792c66556650e6c084b7422aef
Sergei O. Udalov <roger.wilco.from.sq@gmail.com>
2014-04-07 13:19:27 +0400

    табы с графиками

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,19 +4,22 @@ module ApplicationHelper
       "Нет данных"
     else
       content_tag :div do
-        concat content_tag(:div, nil, :id => "metric_graph_#{metric.id}", :style => "width: 100%")
+        concat content_tag(:div, nil, :id => "metric_graph_#{metric.id}", :style => "width: 100%", :class => "dygraph_container")
 
         concat javascript_tag <<-JS
+        var container_id = "metric_graph_#{metric.id}";
         g = new Dygraph(
 
           // containing div
-          document.getElementById("metric_graph_#{metric.id}"),
+          document.getElementById(container_id),
 
           // CSV or path to a CSV file.
           "Date,#{title || metric.title}\\n" +
 
           #{raw metric.data_set.sort_by{ |d| d[:date] }.map{ |d| "\"#{d[:date].strftime("%Y-%m-%d %H:%M")},#{d[:value]}\\n\"" }.join(" + ")}
         );
+
+        $("#" + container_id).data("dygraph", g);
         JS
       end
     end
