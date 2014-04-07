0146c1dc8daa0f4c434659536d9edaaedd65a4bb
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-04-07 09:38:06 +0400

    графики: на главной и для сайта

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require dygraph-combined
 //= require jquery_ujs
 //= require sites
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def metric_graph(metric, title=nil)
+    if metric.blank? || metric.data_set.blank?
+      "Нет данных"
+    else
+      concat content_tag(:div, nil, :id => "metric_graph_#{metric.id}")
+
+      concat javascript_tag <<-JS
+      g = new Dygraph(
+
+        // containing div
+        document.getElementById("metric_graph_#{metric.id}"),
+
+        // CSV or path to a CSV file.
+        "Date,#{title || metric.title}\\n" +
+
+        #{raw metric.data_set.sort_by{ |d| d[:date] }.map{ |d| "\"#{d[:date].strftime("%Y-%m-%d %H:%M")},#{d[:value]}\\n\"" }.join(" + ")}
+      );
+      JS
+    end
+  end
 end
